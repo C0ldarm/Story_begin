150985a53fea0de038308e42f6a920ff482cc88a
C0ldarm <Vladlegend666@gmail.com>
2024-09-10 03:11:17 +0300

    test patch

---
+++ Labs/Typescript/Lab1/variables.js
@@ -0,0 +1,13 @@
+"use strict";
+let stringa = "Hello, TypeScript!";
+let num = 42;
+let goool = true;
+let anything = "This can be anything";
+console.log("String:", stringa);
+console.log("Number:" + num);
+console.log("Boolean:", goool);
+console.log("Any value:" + anything);
+let errors = ["0x000000F4 CRITICAL_OBJECT_TERMINATION", "Kernel Panic - Not Syncing: Fatal Exception", "Machine Check Exception: Bank 1: 0x0000000000000004"];
+let hash = [80070490, 10000007, 10000000000000004, 24, 222222225];
+console.log("errors:", errors);
+console.log("hash:" + hash);

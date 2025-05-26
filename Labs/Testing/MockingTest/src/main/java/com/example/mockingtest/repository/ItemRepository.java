240ad668de07d5723476ae1a4fec7f33b52d94a9
C0ldarm <Vladlegend666@gmail.com>
2025-05-26 22:40:31 +0300

    .

--- Labs/Testing/MockingTest/src/main/java/com/example/mockingtest/repository/ItemRepository.java
+++ Labs/Testing/MockingTest/src/main/java/com/example/mockingtest/repository/ItemRepository.java
@@ -1,13 +1,5 @@
 package com.example.mockingtest.repository;
 
-/*
-  @author   grok
-  @project   mockingtest
-  @class    ItemRepository
-  @version  1.0.0
-  @since    26.05.25 - 22.00
-*/
-
 import com.example.mockingtest.model.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;

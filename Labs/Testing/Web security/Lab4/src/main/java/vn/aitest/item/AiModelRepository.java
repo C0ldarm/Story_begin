02fa6909f29f7054afb705dd8f99cb0702beb01e
C0ldarm <Vladlegend666@gmail.com>
2025-11-02 23:47:02 +0300

    fail

--- Labs/Testing/Web security/Lab4/src/main/java/vn/aitest/item/AiModelRepository.java
+++
@@ -1,8 +0,0 @@
-package vn.aitest.item;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AiModelRepository extends MongoRepository<AiModel, String> {
-}

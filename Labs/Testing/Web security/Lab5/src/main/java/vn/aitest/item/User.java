81f1f69122bb332a17950e4ad3643778d4125e89
C0ldarm <Vladlegend666@gmail.com>
2025-11-05 01:14:06 +0300

    Added Lab5: Migrated AiTest project with MongoDB AiModel CRUD, multi-user RBAC (USER/PUBLISHER/ADMIN), Basic Auth, BCrypt

---
+++ Labs/Testing/Web security/Lab5/src/main/java/vn/aitest/item/User.java
@@ -0,0 +1,54 @@
+package vn.aitest.item;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Document(collection = "users")
+public class User implements UserDetails {
+
+    public enum Role {
+        USER, PUBLISHER, ADMIN
+    }
+
+    @Id
+    private String id;
+    private String username;
+    private String password;
+    private List<Role> roles;
+
+    public User() {}
+
+    public User(String username, String password, List<Role> roles) {
+        this.username = username;
+        this.password = password;
+        this.roles = roles;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role.name()))
+                .toList();
+    }
+
+    @Override public String getPassword() { return password; }
+    @Override public String getUsername() { return username; }
+    @Override public boolean isAccountNonExpired() { return true; }
+    @Override public boolean isAccountNonLocked() { return true; }
+    @Override public boolean isCredentialsNonExpired() { return true; }
+    @Override public boolean isEnabled() { return true; }
+
+    // Getters & Setters
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+    public void setUsername(String username) { this.username = username; }
+    public void setPassword(String password) { this.password = password; }
+    public List<Role> getRoles() { return roles; }
+    public void setRoles(List<Role> roles) { this.roles = roles; }
+}
